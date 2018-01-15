a1f67843de5df849cf29261a45b32319a8833ffb
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2018-01-15 07:10:41 -0500

    9.3.4

--- compulsive-traders/src/main/java/io/vertx/workshop/trader/impl/JavaCompulsiveTraderVerticle.java
+++ compulsive-traders/src/main/java/io/vertx/workshop/trader/impl/JavaCompulsiveTraderVerticle.java
@@ -2,6 +2,7 @@ package io.vertx.workshop.trader.impl;
 
 import io.vertx.core.CompositeFuture;
 import io.vertx.core.Future;
+import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.eventbus.MessageConsumer;
 import io.vertx.core.json.JsonObject;
 import io.vertx.servicediscovery.types.EventBusService;
@@ -23,6 +24,27 @@ public class JavaCompulsiveTraderVerticle extends MicroServiceVerticle {
     String company = TraderUtils.pickACompany();
     int numberOfShares = TraderUtils.pickANumber();
     System.out.println("Java compulsive traders configured for company " + company + " and shares " + numberOfShares);
+
+    Future<PortfolioService> portfolioServiceFuture = Future.future();
+    Future<MessageConsumer<JsonObject>> marketFuture = Future.future();
+
+    MessageSource.getConsumer(discovery, new JsonObject().put("name", "market-data"), marketFuture.completer());
+    EventBusService.getProxy(discovery, PortfolioService.class, portfolioServiceFuture.completer());
+
+    CompositeFuture.all(marketFuture, portfolioServiceFuture).setHandler(ar -> {
+      if (ar.failed()) {
+        future.fail("One of the required services cannot be retrieved: " + ar.cause());
+      }else {
+        PortfolioService portfolioService = portfolioServiceFuture.result();
+        MessageConsumer<JsonObject> marketConsumer = marketFuture.result();
+
+        marketConsumer.handler(message -> {
+          JsonObject quote = message.body();
+          TraderUtils.dumbTradingLogic(company, numberOfShares, portfolioService, quote);
+        });
+        future.complete();
+      }
+    });
     // ----
   }
 
